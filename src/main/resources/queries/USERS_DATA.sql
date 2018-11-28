bb1ad49e74a0a76218729120e01554240bcf76e8
Tadeo Rivera <xtecuan@gmail.com>
2018-11-28 04:48:06 -0600

    h2-database-service : H2 Database Service Listener

---
+++ src/main/resources/queries/USERS_DATA.sql
@@ -0,0 +1,6 @@
+INSERT INTO PUBLIC.USERS_TABLE (EMAIL, FIRSTNAME, MIDDLENAME, LASTNAME) VALUES('xtecuanufo@gmail.com', 'Tadeo', '', 'Rivera-Pineda');
+INSERT INTO PUBLIC.USERS_TABLE (EMAIL, FIRSTNAME, MIDDLENAME, LASTNAME) VALUES('xtecuan@protonmail.com', 'Julian', '', 'Rivera-Pineda');
+INSERT INTO PUBLIC.USERS_TABLE (EMAIL, FIRSTNAME, MIDDLENAME, LASTNAME) VALUES('juliux@gmail.com', 'Julio', 'Adalberto', 'Rivera-Pineda');
+INSERT INTO PUBLIC.USERS_TABLE (EMAIL, FIRSTNAME, MIDDLENAME, LASTNAME) VALUES('kingofkings1982@live.com', 'Jonatan', '', 'Rivera-Pineda');
+INSERT INTO PUBLIC.USERS_TABLE (EMAIL, FIRSTNAME, MIDDLENAME, LASTNAME) VALUES('marvinposada@gmail.com', 'Marvin', 'Ernesto', 'Posada Joaquin');
+INSERT INTO PUBLIC.USERS_TABLE (EMAIL, FIRSTNAME, MIDDLENAME, LASTNAME) VALUES('melvynraul@gmail.com', 'Melvyn', 'Raúl', 'Gómez Guevara');
